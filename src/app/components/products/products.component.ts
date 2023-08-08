7e79d90d4f55ebfc06b184fe349e56a922337d48
piandas <santiagobustosp@gmail.com>
2023-08-08 18:24:56 -0500

    Solicitudes Post

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Product } from '../../models/product.model';
+import { Product, CreateProductDTO } from '../../models/product.model';
 
 import { StoreService } from '../../services/store.service';
 import { ProductsService } from '../../services/products.service';
@@ -56,6 +56,20 @@ export class ProductsComponent implements OnInit {
     .subscribe(data => {
       this.toggleProductDetail();
       this.productChosen = data;
+    })
+  }
+
+  createNewProduct() {
+    const product: CreateProductDTO = {
+      title: 'Nuevo producto',
+      description: 'Bla bla bla',
+      images: [`https://picsum.photos/200`],
+      price: 1000,
+      categoryId: 2,
+    }
+    this.productsService.create(product)
+    .subscribe(data => {
+      this.products.unshift(data);
     })
   }
 
