c75e7f97aebf12fd48380704e99ffecfadc11bc3
piandas <santiagobustosp@gmail.com>
2023-08-08 20:51:10 -0500

    Delete

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -86,4 +86,13 @@ export class ProductsComponent implements OnInit {
     })
   }
 
+  deleteProduct() {
+    const id = this.productChosen.id;
+    this.productsService.delete(id)
+    .subscribe(() => {
+      const productIndex = this.products.findIndex(item => item.id === this.productChosen.id);
+      this.products.splice(productIndex, 1);
+      this.showProductDetail = false;
+    });
+  }
 }
